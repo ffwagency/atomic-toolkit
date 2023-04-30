965059cbc9aa73f2c9846b4d8278ea746c6cb1a6
dydimitrov <dimitar.dimitrov@ffw.com>
2023-04-30 17:43:18 +0300

    small changes

---
+++ src/AtomicStarterKit/Components/DoubleImageViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace AtomicStarterKit.Components;
+
+[ViewComponent(Name = DoubleImage.ModelTypeAlias)]
+public class DoubleImageViewComponent : ViewComponent
+{
+	public IViewComponentResult Invoke(DoubleImage source)
+	{
+		return View("~/Views/Components/DoubleImage.cshtml", source);
+	}
+}
---
+++ src/AtomicStarterKit/Components/QuoteViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace AtomicStarterKit.Components;
+
+[ViewComponent(Name = Quote.ModelTypeAlias)]
+public class QuoteViewComponent : ViewComponent
+{
+	public IViewComponentResult Invoke(Quote source)
+	{
+		return View("~/Views/Components/Quote.cshtml", source);
+	}
+}
