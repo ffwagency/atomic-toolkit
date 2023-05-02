a5319e2686a6ab091272dec97dfc9109bc121c39
dydimitrov <dimitar.dimitrov@ffw.com>
2023-05-02 15:17:56 +0300

    bakc to separate js files

--- src/AtomicStarterKit.Website/wwwroot/atomic-kit-assets/js/atomic-kit.js
+++ src/AtomicStarterKit.Website/wwwroot/atomic-kit-assets/js/atomic-kit.js
@@ -117,28 +117,4 @@
         });
     }
 
-    if (document.querySelectorAll('[data-bss-baguettebox]').length > 0) {
-        baguetteBox.run('[data-bss-baguettebox]', { animation: 'slideIn' });
-    }
-
-    document.addEventListener("DOMContentLoaded", function () {
-
-        // Initializing the swiper plugin for the slider.
-        // Read more here: http://idangero.us/swiper/api/
-
-        var mySwiper = new Swiper('.swiper-container', {
-            loop: true,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true
-            },
-            paginationClickable: true,
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev'
-            }
-        });
-
-    });
-
 })(); // End of use strict
