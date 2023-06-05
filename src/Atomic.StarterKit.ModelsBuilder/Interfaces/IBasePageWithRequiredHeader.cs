507d51bae97529ab1b973c184f1b7034711bbdce
darkosubic-ffw <darko.subic@ffwagency.com>
2023-06-05 15:56:24 +0200

    Atomic.StarterKit.ModelsBuilder.Interfaces.IBasePageWithRequiredHeader is no longer a partial class

--- src/Atomic.StarterKit.ModelsBuilder/Interfaces/IBasePageWithRequiredHeader.cs
+++ src/Atomic.StarterKit.ModelsBuilder/Interfaces/IBasePageWithRequiredHeader.cs
@@ -2,7 +2,7 @@ using Umbraco.Cms.Core.Models.PublishedContent;
 
 namespace Atomic.StarterKit.ModelsBuilder.Interfaces;
 
-public partial interface IBasePageWithRequiredHeader : IPublishedContent
+public interface IBasePageWithRequiredHeader : IPublishedContent
 {
     Umbraco.Cms.Core.Models.Blocks.BlockListModel PageHeader { get; }
 }
