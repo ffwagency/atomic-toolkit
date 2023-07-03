6bce7642d3b66c17a442cbc50dda901f98a4808b
Rosen Petrov <rosen.petrov@ffw.com>
2023-07-03 16:13:10 +0300

    StarterKit and Seo release

--- src/Atomic.Seo/Migrations/SeoPackageMigrationPlan.cs
+++ src/Atomic.Seo/Migrations/SeoPackageMigrationPlan.cs
@@ -8,6 +8,6 @@ public class AtomicStarterKitSEOPackageMigrationPlan : PackageMigrationPlan
 
 	protected override void DefinePlan()
 	{
-		To<ImportPackageXmlMigration>("D6C32ED91DBD4BF4A8A7F3F26F59166B");
+		To<ImportPackageXmlMigration>("D6C32ED91DBD4BF4A8A7F3F26F59165A");
 	}
 }
